75b4465312645d7c561a1de66768d01e12bcaac5
ulysseskao <ulysseskao@ximple.com.tw>
2018-08-28 01:46:24 +0800

    客製化部分

---
+++ projects/tools/src/lib/feature-detail-tool/feature-detail-tool.component.ts
@@ -0,0 +1,19 @@
+import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+
+import { Register } from '@ximple/igo2-context';
+import { FeatureService } from '@ximple/igo2-geo';
+
+
+@Register({
+  name: 'featureDetails',
+  title: 'igo.featureDetails',
+  icon: 'bookmark'
+})
+@Component({
+  selector: 'igo-feature-detail-tool',
+  templateUrl: './feature-detail-tool.component.html'
+})
+export class FeatureDetailToolComponent {
+  constructor(public featureService: FeatureService) {
+  }
+}
