67eee3becc602b970e09eb8169b8bf397173e827
ulysseskao <ulysseskao@ximple.com.tw>
2018-08-28 11:19:11 +0800

    針對publish調整以及修正async錯誤

--- projects/tools/src/lib/feature-detail-tool/feature-detail-tool.module.ts
+++ projects/tools/src/lib/feature-detail-tool/feature-detail-tool.module.ts
@@ -4,15 +4,17 @@ import {
   CUSTOM_ELEMENTS_SCHEMA
 } from '@angular/core';
 
+import { CommonModule } from '@angular/common';
+
 import { IgoFeatureModule } from '@ximple/igo2-geo';
 import { FeatureDetailToolComponent } from './feature-detail-tool.component';
 
 @NgModule({
-  imports: [IgoFeatureModule],
-  declarations: [FeatureDetailToolComponent],
-  exports: [FeatureDetailToolComponent],
-  entryComponents: [FeatureDetailToolComponent],
-  schemas: [CUSTOM_ELEMENTS_SCHEMA]
+  imports: [ CommonModule, IgoFeatureModule ],
+  declarations: [ FeatureDetailToolComponent ],
+  exports: [ FeatureDetailToolComponent ],
+  entryComponents: [ FeatureDetailToolComponent ],
+  schemas: [ CUSTOM_ELEMENTS_SCHEMA ]
 })
 export class IgoFeatureDetailToolModule {
   static forRoot(): ModuleWithProviders {
