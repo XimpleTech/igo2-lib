8ecca16ce92a90f7ade1cb7d4a93c106923563ad
ulysseskao <ulysseskao@ximple.com.tw>
2018-09-03 10:54:43 +0800

    fix(auth): fix login/logout/alreadyLogin div

--- projects/auth/src/lib/auth-form/auth-form.component.ts
+++ projects/auth/src/lib/auth-form/auth-form.component.ts
@@ -19,16 +19,10 @@ import { AuthService } from '../shared/auth.service';
 })
 export class AuthFormComponent implements OnInit {
   @Input()
-  get alreadyConnectedDiv(): boolean {
-    return this._alreadyConnectedDiv;
-  }
-  set alreadyConnectedDiv(value: boolean) {
-    this._alreadyConnectedDiv = value.toString() === 'true';
-  }
-  private _alreadyConnectedDiv = false;
-
-  @Input()
   get backgroundDisable(): boolean {
+    if (this.isLogoutRoute || this.isLogoutRoute) {
+      return false;
+    }
     return this._backgroundDisable;
   }
   set backgroundDisable(value: boolean) {
@@ -36,10 +30,61 @@ export class AuthFormComponent implements OnInit {
   }
   private _backgroundDisable = true;
 
+  @Input()
+  get hasAlreadyConnectedDiv(): boolean {
+    return this._hasAlreadyConnectedDiv;
+  }
+  set hasAlreadyConnectedDiv(value: boolean) {
+    this._hasAlreadyConnectedDiv = value.toString() === 'true';
+  }
+  private _hasAlreadyConnectedDiv = true;
+
+  @Input()
+  get hasLogoutDiv(): boolean {
+    return this._hasLogoutDiv;
+  }
+  set hasLogoutDiv(value: boolean) {
+    this._hasLogoutDiv = value.toString() === 'true';
+  }
+  private _hasLogoutDiv = true;
+
+  @Input()
+  get showAlreadyConnectedDiv(): boolean {
+    if (this.isLogoutRoute) {
+      return this.hasAlreadyConnectedDiv;
+    }
+    return this._showAlreadyConnectedDiv;
+  }
+  set showAlreadyConnectedDiv(value: boolean) {
+    this._showAlreadyConnectedDiv = value.toString() === 'true';
+  }
+  private _showAlreadyConnectedDiv = false;
+
+  @Input()
+  get showLogoutDiv(): boolean {
+    if (this.isLogoutRoute) {
+      return this.hasLogoutDiv;
+    }
+    return this._showLogoutDiv;
+  }
+  set showLogoutDiv(value: boolean) {
+    this._showLogoutDiv = value.toString() === 'true';
+  }
+  private _showLogoutDiv = false;
+
+  get showLoginDiv(): boolean {
+    if (!this.isLogoutRoute) {
+      return true;
+    }
+  }
+
   public options: AuthOptions;
   public user;
 
   public visible = true;
+
+  private isLoginRoute: boolean;
+  private isLogoutRoute: boolean;
 
   constructor(
     public auth: AuthService,
@@ -48,28 +93,44 @@ export class AuthFormComponent implements OnInit {
   ) {
     this.options = this.config.getConfig('auth') || {};
     this.visible = Object.getOwnPropertyNames(this.options).length !== 0;
-
-    if (this.auth.decodeToken()) {
-      this.user = {
-        name: this.auth.decodeToken().user.sourceId
-      };
-    }
   }
 
   public ngOnInit() {
     this.analyzeRoute();
+    this.getName();
   }
 
   public login() {
     this.auth.goToRedirectUrl();
+    this.getName();
   }
 
   public logout() {
     this.auth.logout().subscribe(() => {
-      if (this.router && this.options.loginRoute) {
-        this.router.navigate([this.options.loginRoute]);
+      this.user = undefined;
+      if (this.router) {
+        if (this.options.logoutRoute) {
+          this.router.navigate([this.options.logoutRoute]);
+        } else if (this.options.homeRoute) {
+          this.router.navigate([this.options.homeRoute]);
+        }
       }
     });
+  }
+
+  public home() {
+    if (this.router && this.options.homeRoute) {
+      this.router.navigate([this.options.homeRoute]);
+    }
+  }
+
+  private getName() {
+    if (this.auth.decodeToken()) {
+      const tokenDecoded = this.auth.decodeToken();
+      this.user = {
+        name: tokenDecoded.user.firstName || tokenDecoded.user.sourceId
+      };
+    }
   }
 
   private analyzeRoute() {
@@ -81,14 +142,21 @@ export class AuthFormComponent implements OnInit {
     const loginRoute = this.options.loginRoute;
     const currentRoute = this.router.url;
 
-    const isLogoutRoute: boolean = currentRoute === logoutRoute;
-    const isLoginRoute: boolean = currentRoute === loginRoute;
+    this.router.events
+      .pipe(filter(event => event instanceof NavigationStart))
+      .subscribe((changeEvent: any) => {
+        if (changeEvent.url) {
+          const currentRoute = changeEvent.url;
+          const logoutRoute = this.options.logoutRoute;
+          const loginRoute = this.options.loginRoute;
 
-    if (isLogoutRoute) {
-      this.logout();
-    } else if (isLoginRoute) {
-      this.backgroundDisable = false;
-      this.alreadyConnectedDiv = true;
-    }
+          this.isLogoutRoute = currentRoute === logoutRoute;
+          this.isLoginRoute = currentRoute === loginRoute;
+
+          if (this.isLogoutRoute) {
+            this.auth.logout();
+          }
+        }
+      });
   }
 }
